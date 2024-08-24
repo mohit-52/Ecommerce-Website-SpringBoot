746ff949f6d0052785dc643c449cfed6288f9e4c
Mohit <mohitkaushis7@gmail.com>
2024-08-24 11:47:14 +0530

    First Commit

---
+++ ecom-proj/ecom-proj/src/main/java/com/mohit/ecom_proj/modal/Product.java
@@ -0,0 +1,37 @@
+package com.mohit.ecom_proj.modal;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private  String brand;
+    private BigDecimal price;
+    private String category;
+//    @JsonFormat(shape =  JsonFormat.Shape.STRING, pattern = "dd-MM-yyy") --> Handled in UI
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageData;
+
+}
---
+++ ecom-proj/ecom-proj/src/main/resources/data1.sql
@@ -0,0 +1,7 @@
+INSERT INTO Product (name, desc, brand, price, category, release_date, available, quantity, imageName, imageType, imageData )
+VALUES
+    ('Model S', 'Electric sedan', 'Tesla', 79999.99, 'Sedan', '2023-01-15', true, 50,null,null,null),
+    ('Mustang GT', 'Muscle car', 'Ford', 55999.99, 'Coupe', '2022-11-10', true, 30,null,null,null),
+    ('Civic', 'Compact sedan', 'Honda', 21999.99, 'Sedan', '2023-05-20', true, 100,null,null,null),
+    ('Camry', 'Midsize sedan', 'Toyota', 24999.99, 'Sedan', '2023-03-12', true, 80,null,null,null),
+    ('Wrangler', 'Off-road SUV', 'Jeep', 32999.99, 'SUV', '2023-07-05', true, 25,null,null,null);
